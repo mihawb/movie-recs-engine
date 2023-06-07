a5454a6c3472d2d746beda3ca4ecc8564b9042e0
mihawb <michalbanaszczak@gmail.com>
2023-06-07 02:35:14 +0200

    api cleanup, readme

--- flask-api/README.md
+++ flask-api/README.md
@@ -1,10 +1,88 @@
-# Deploy a Python (Flask) web app to Azure App Service - Sample Application
+# movie-rex API
 
-This is the sample Flask application for the Azure Quickstart [Deploy a Python (Django or Flask) web app to Azure App Service](https://docs.microsoft.com/en-us/azure/app-service/quickstart-python). For instructions on how to create the Azure resources and deploy the application to Azure, refer to the Quickstart article.
+### `/api/recommendations`
+Takes a list of movie IDs and returns a list of recommendations' IDs.
+Returns an empty list on a cold start (empty input).
+Sample query: [`/api/recommendations?id=m238&id=m80`](https://movie-rex.azurewebsites.net/api/recommendations?id=m238&id=m80)
+```
+[
+	"m15804",
+	"m8422",
+	"m25376",
+	"m152742",
+	"m6075",
+	"m1280",
+	"m678",
+	"m8073",
+	"m17057",
+	"m3133"
+]
+```
 
-Sample applications are available for the other frameworks here:
+### `/api/rexwithinfo`
+Same business logic as above, with human-readable response format.
+Sample query: [`/api/rexwithinfo?id=m238&id=m80`](https://movie-rex.azurewebsites.net/api/rexwithinfo?id=m238&id=m80)
+```
+[
+	{
+		"genres": [
+			"Crime",
+			"Drama",
+			"Romance"
+		],
+		"id": "m15804",
+		"release_date": "1991-07-27",
+		"title": "A Brighter Summer Day"
+	},
+	{
+		"genres": [
+			"Crime",
+			"Drama",
+			"Romance"
+		],
+		"id": "m8422",
+		"release_date": "1960-10-06",
+		"title": "Rocco and His Brothers"
+},
+	...
+]
+```
 
-* Django [https://github.com/Azure-Samples/msdocs-python-django-webapp-quickstart](https://github.com/Azure-Samples/msdocs-python-django-webapp-quickstart)
-* FastAPI [https://github.com/Azure-Samples/msdocs-python-fastapi-webapp-quickstart](https://github.com/Azure-Samples/msdocs-python-fastapi-webapp-quickstart)
+### `/api/info`
+Returns all vertex properties from Gremlin DB. Is vectorized.
+Sample query: [`/api/info?id=m49051)`](https://movie-rex.azurewebsites.net/api/info?id=m49051)
+```
+[
+	{
+		"adult": false,
+		"backdrop_path": "/xyXmtuvsoM5J3yNad0nvcetpBdY.jpg",
+		"genre_ids": [
+			12,
+			14,
+			28
+		],
+		"genres": [
+			"Adventure",
+			"Fantasy",
+			"Action"
+		],
+		"id": "m49051",
+		"label": "movie",
+		"original_language": "en",
+		"original_title": "The Hobbit: An Unexpected Journey",
+		"pk": "pk",
+		"popularity": 49.254,
+		"poster_path": "/yHA9Fc37VmpUA5UncTxxo3rTGVA.jpg",
+		"release_date": "2012-11-26",
+		"title": "The Hobbit: An Unexpected Journey",
+		"video": false,
+		"vote_average": 7.3,
+		"vote_count": 17028
+	}
+]
+```
 
-If you need an Azure account, you can [create one for free](https://azure.microsoft.com/en-us/free/).
+### `/api/random`
+`count` defaults to `20`
+Returns a list of movie objects, as seen in `/api/info`. Could be used as a starting point for a hypothetical external module or website.
+Sample query: [`/api/random?count=13`](https://movie-rex.azurewebsites.net/api/random?count=13) (click and check out in browser)
--- flask-api/app.py
+++ flask-api/app.py
@@ -1,11 +1,7 @@
-import os
 from gremlin.connector import *
 from gremlin_python.driver import client, serializer
-import asyncio
 from os import environ
-
-from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for, jsonify)
+from flask import (Flask, redirect, request, url_for, jsonify)
 
 
 GREMLIN_ENDPOINT = environ['GREMLIN_ENDPOINT']
@@ -27,43 +23,12 @@ client = client.Client(
 )
 
 
-@app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
-
-@app.route('/hello', methods=['POST'])
-def hello():
-   name = request.form.get('name')
- 
-   if name:
-       print('Request for hello page received with name=%s' % name)
-       movie_obj = get_vertex_properties(client, 'm238') # connector used here
-       return render_template('hello.html', recommendations = movie_obj[0]['title'])
-   else:
-       print('Request for hello page received with no name or blank name -- redirecting')
-       return redirect(url_for('index'))
-
-
-@app.route('/api/random', methods=['GET'])
-def api_get_random_movies():
-    count = request.args.get('count', 20)
-    random_movies = get_random_movies(client, count)
-    return jsonify(random_movies)
-
-
-@app.route('/api/info', methods=['GET'])
-def api_get_info():
-    m_id = request.args.getlist('id')
-    m_obj_list = get_vertex_properties(client, m_id)
-    return jsonify(m_obj_list)
+def _make_human_readable(m_obj: dict) -> dict:
+    tmp = list(m_obj.keys())
+    for key in tmp:
+        if key not in ('title', 'genres', 'id', 'release_date'):
+            m_obj.pop(key)
+    return m_obj
 
 
 def _generate_recommendations(m_ids: list[str]) -> list[str]:
@@ -87,19 +52,32 @@ def _generate_recommendations(m_ids: list[str]) -> list[str]:
     return rex[:10]
 
 
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return redirect(url_for('api_get_random_movies'))
+
+
+@app.route('/api/random', methods=['GET'])
+def api_get_random_movies():
+    count = request.args.get('count', 20)
+    random_movies = get_random_movies(client, count)
+    return jsonify(random_movies)
+
+
+@app.route('/api/info', methods=['GET'])
+def api_get_info():
+    m_id = request.args.getlist('id')
+    m_obj_list = get_vertex_properties(client, m_id)
+    return jsonify(m_obj_list)
+
+
+
 @app.route('/api/recommendations', methods=['GET'])
 def api_get_recommendations():
     m_ids = request.args.getlist('id')
     rex = _generate_recommendations(m_ids)
     return jsonify(rex)
-
-
-def _make_human_readable(m_obj: dict) -> dict:
-    tmp = list(m_obj.keys())
-    for key in tmp:
-        if key not in ('title', 'genres', 'id', 'release_date'):
-            m_obj.pop(key)
-    return m_obj
 
 
 @app.route('/api/rexwithinfo', methods=['GET'])
